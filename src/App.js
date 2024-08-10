6c47523f7e0c45fe5f54e98d57f9178ac57178ad
FahadAkash <fahad12345ff8@gmail.com>
2024-08-10 16:35:01 +0600

    Main Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-function App() {
+const App = () => {
+  const API_URL = "http://www.omdbapi.com/?apikey="+process.API_KEY;
+
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const movie1 = {
+    "imdbID" : "1212" , 
+    "Poster" : "122324" ,
+    "Title" : "SpiderMan"
+  }
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('Spider');
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Movie Finder</h1>
+      <div className="search">
+        <input
+          placeholder='Search For Movie'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='Search'
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {
+        movies?.length > 0 ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard key={movie.imdbID} movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className='container'>
+            <MovieCard movie={movie1} />
+          </div>
+        )
+      }
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/person.js
@@ -0,0 +1,17 @@
+const Person = (probs) =>{
+    const PerName = probs.name ;
+    const perAge = probs.age;
+    const perNationality = probs.nationality;
+return(
+
+    <>
+     <h1>Name : {PerName}</h1>
+    <h1>Age : {perAge}</h1>
+
+
+  </>
+
+)
+    
+}
+export default Person;
